19386d13cf0f34251a1a78df1ad1812a2e6f9214
KoushikVtalent <koushik.vtalent@gmail.com>
2019-12-19 17:43:09 +0530

    Addition Method added in operations class

---
+++ TestProjects/BussinessAccessLayer/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProjects.BussinessAccessLayer
+{
+    public class Operations
+    {
+        public int Addition()
+        {
+            int a=10;
+            int b=20;
+            int c = a + b;
+            return c;
+
+        }
+    }
+}
