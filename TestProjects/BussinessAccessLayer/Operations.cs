45a946cfd730b3ec86b6df5b74a9d1d087f98c7b
KoushikVtalent <koushik.vtalent@gmail.com>
2019-12-19 17:46:56 +0530

    Made the addition method Dynamic

--- TestProjects/BussinessAccessLayer/Operations.cs
+++ TestProjects/BussinessAccessLayer/Operations.cs
@@ -7,12 +7,9 @@ namespace TestProjects.BussinessAccessLayer
 {
     public class Operations
     {
-        public int Addition()
+        public int Addition(int a ,int b)
         {
-            int a=10;
-            int b=20;
-            int c = a + b;
-            return c;
+            return a + b;
 
         }
     }
